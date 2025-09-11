0cf00f270f0b111ba62a38ceef19a7ac0b5ae126
El Pruzen <muhammadfauzan9981@gmail.com>
2025-09-11 15:37:02 +0700

    Inisilisasi

---
+++ src/modul_1/Main.java
@@ -0,0 +1,8 @@
+package modul_1;
+
+public class Main {
+    // Main itu fungsi khusus sebagai entry point dari aplikasi yang kita buat
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
